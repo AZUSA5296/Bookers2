8d199b2df341805c2dc6817165349e89b01bc1d1
AZUSA5296 <nnfnnfnakuyo731@gmail.com>
2021-05-22 15:22:15 +0000

    [finish]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c1982b98d6db040f860b2f17a92ea5dcdbedf8e5d653035ab813907b259d9c362e82961040b302db574821b936ae2a7f984cee0e960122f3f0b182f2df925a37'
